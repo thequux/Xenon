1697f6c14bac46de21bed54d0453992e71b381bf
root <root>
2006-11-30 01:06:09 +0000

    Made massive VGA console branch

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #qemu -fda boot.img -boot a -m 32  c.img -std-vga 
-qemu -fda boot.img -boot a -m 32  c.img -std-vga -serial tcp::2434 -serial tcp::2435 -s
+qemu -fda boot.img -boot a -m 32  c.img -std-vga # -serial tcp::2434 -serial tcp::2435 -s
 
